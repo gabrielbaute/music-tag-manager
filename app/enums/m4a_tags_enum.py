c36fcb6b3f55065a01877a3ccee18208b48cb6d0
gabrielbaute <gabrielbaute@gmail.com>
2026-07-05 10:05:57 -0400

    Enum para los tags del estandar mp4

---
+++ app/enums/m4a_tags_enum.py
@@ -0,0 +1,21 @@
+from typing import List
+from enum import StrEnum
+
+class M4ATagEnum(StrEnum):
+    TRACK_TITLE ='\xa9nam'
+    ALBUM ='\xa9alb'
+    ARTISTS ='\u00a9ART'
+    ALBUM_ARTISTS ='aART'
+    COMPOSER ='\xa9wrt'
+    GENRES ='\u00a9gen'
+    WORK ='\xa9wrk'
+    MOVEMENT ='\xa9mvn'
+    TRACK_NUMBER ='trkn'
+    DISC_NUMBER ='disk'
+    LYRICS ='\xa9lyr'
+
+    def __str__(self):
+        return self.value
+    
+    def to_list(self) -> List[str]:
+        return [self.value]
