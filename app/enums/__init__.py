1765dfc2a247c3ef5447a1c16763063f5532b02f
gabrielbaute <gabrielbaute@gmail.com>
2026-07-06 13:54:49 -0400

    Agregamos los enums para llevar el control de los archivos editados

--- app/enums/__init__.py
+++ app/enums/__init__.py
@@ -1,4 +1,6 @@
 from app.enums.format_enum import Format
+from app.enums.item_type_enum import ItemType
 from app.enums.m4a_tags_enum import M4ATagEnum
 from app.enums.mp3_tags_enum import MP3TagEnum
+from app.enums.edit_status_enum import EditStatus
 from app.enums.id3_versions_enum import ID3VersionEnum
