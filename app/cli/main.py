ba15925c2d278d98c378c7f64ea925d6214ea20d
gabrielbaute <gabrielbaute@gmail.com>
2026-07-06 15:42:20 -0400

    Parser y función principal de la CLI

---
+++ app/cli/main.py
@@ -0,0 +1,44 @@
+import sys
+from pathlib import Path
+from rich.console import Console
+
+from app.cli.cli_parser import create_parser
+from app.cli.commands import (
+    show_version,
+    analyze_command,
+    set_genres_to_album_command,
+    swap_artists_command,
+    force_album_artist_command,
+    sanitize_command,
+    fix_field_command,
+)
+
+def main() -> None:
+    parser = create_parser()
+    args = parser.parse_args()
+
+    console = Console()
+
+    if args.command == "version":
+        show_version(console)
+
+    elif args.command == "analyze":
+        analyze_command(console, args)
+
+    elif args.command == "genres":
+        set_genres_to_album_command(console, args)
+
+    elif args.command == "swap-artists":
+        swap_artists_command(console, args)
+
+    elif args.command == "force-album-artist":
+        force_album_artist_command(console, args)
+
+    elif args.command == "sanitize":
+        sanitize_command(console, args)
+
+    elif args.command == "fix-field":
+        fix_field_command(console, args)
+
+    else:
+        parser.print_help()
