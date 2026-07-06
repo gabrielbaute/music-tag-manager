3a616bb6808f624f317851c212b2dedb28d0f97d
gabrielbaute <gabrielbaute@gmail.com>
2026-07-06 15:08:32 -0400

    Agregamos el campo RELASE_DATE al enum equivalente a YEAR

--- app/enums/m4a_tags_enum.py
+++ app/enums/m4a_tags_enum.py
@@ -2,6 +2,11 @@ from typing import List
 from enum import StrEnum
 
 class M4ATagEnum(StrEnum):
+    """
+    Enum que mapea las etiquetas lógicas con el valor en el diccionario de Mutagen.
+
+    Establece las equivalencias necesarias para interactuar con la interfaz nativa de Mutagen MP4 utilizando los identificadores estándar.
+    """
     TRACK_TITLE ='\xa9nam'
     ALBUM ='\xa9alb'
     ARTISTS ='\u00a9ART'
@@ -13,6 +18,7 @@ class M4ATagEnum(StrEnum):
     TRACK_NUMBER ='trkn'
     DISC_NUMBER ='disk'
     LYRICS ='\xa9lyr'
+    RELEASE_DATE='\xa9day'
 
     def __str__(self):
         return self.value
