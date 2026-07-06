14089c55f75c441a391cd363f940aa1f1869ee8a
gabrielbaute <gabrielbaute@gmail.com>
2026-07-06 15:37:42 -0400

    Comando que muestra la versión del proyecto

---
+++ app/cli/commands/version_command.py
@@ -0,0 +1,29 @@
+"""
+Version command for the CLI.
+"""
+from rich.console import Console
+from rich.table import Table
+
+from app.settings.app_version import __version__
+
+def show_version(console: Console) -> None:
+    """
+    Shows the CLI version
+    """
+    console = Console()
+    table = Table(
+        title="[bold magenta]anime-light[/bold magenta]",
+        show_header=False,
+        border_style="blue",
+        padding=(0, 2),
+    )
+    table.add_column("Key", style="cyan", justify="right")
+    table.add_column("Value", style="green")
+    
+    table.add_row("[yellow]Build[/yellow]", "[bold]stable[/bold]")
+    table.add_row("Versión", f"[bold]{__version__}[/bold]")
+    table.add_row("Author", "Gabriel Baute")
+    table.add_row("License", "GPL")
+    table.add_row("Repo", "https://github.com/gabrielbaute/repo-link") #TODO: crear repositorio en github
+
+    console.print(table)
